6383e736576f2872f257fadcc27b7138f82ee1d2
Vasanth Kumar <mybusiness.vkrs@gmail.com>
2025-03-23 14:43:41 +0530

    uploaded

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+function handleForgetPassword() {
+  console.log("Handle Forget password");
+}
+function handleSignup() {
+  window.location.href = "./signup.html";
+}
+function handleForgetPassword() {
+  console.log("Handle Forget password");
+}
+function handleLogin() {
+  window.location.href = "../index.html";
+}
