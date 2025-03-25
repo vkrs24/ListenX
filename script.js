dec65cf6a3247f7ab5b3f5314590140f103a6705
Vasanth Kumar <mybusiness.vkrs@gmail.com>
2025-03-25 09:30:47 +0530

    deleted the backend

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-function handleForgetPassword() {
-  console.log("Handle Forget password");
-}
 function handleSignup() {
   window.location.href = "./signup.html";
-}
-function handleForgetPassword() {
-  console.log("Handle Forget password");
 }
 function handleLogin() {
   window.location.href = "./index.html";
